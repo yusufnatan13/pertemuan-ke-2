ff26547f822e26b22b78235be9d7d3cde3e302ba
yusufnatan13 <112594896+yusufnatan13@users.noreply.github.com>
2022-09-08 09:32:31 +0700

    Pertemuan ke 2
    semangat :)

---
+++ YUSUFARIFIN_2217051158.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+	float luas,panjang,lebar;
+	cout<<"PROGAM C++ MENGHITUNG LUAS DAN PERSEGI PANJANG "<<endl;
+	
+	cout<<"------------------"<<endl;
+	cout<<"masukan panjang/t/t: ";
+	cin>>panjang;
+	cout<<"masukan lebar/t/t";
+	cin>>lebar;
+	keliling=2*(panjang+lebar);
+	cout<<"keliling persegi panjang: "
+	<<keliling<<endl;
+	
+	return 0;
+}
